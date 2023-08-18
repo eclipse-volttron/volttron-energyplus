90c01cf1737a8445ffbb64e47ad6ae170d7cf7bf
riley206 <riley.maltos@pnnl.gov>
2023-08-18 11:59:48 -0700

    Add EnergyPlus Simulation Agent to VOLTTRON

---
+++ src/integrations/__init__.py
@@ -0,0 +1,6 @@
+
+from .gridappsd_integration import GridAPPSDSimIntegration
+from.energyplus_integration import EnergyPlusSimIntegration
+
+
+__all__ = ['GridAPPSDSimIntegration', 'EnergyPlusSimIntegration']
