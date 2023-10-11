7eef6adb32e2e5046b76238a99a7b0093acc7a0a
riley206 <riley.maltos@pnnl.gov>
2023-10-11 11:30:26 -0700

    removed GridAPPSDSimIntegration

--- src/integrations/__init__.py
+++ src/integrations/__init__.py
@@ -1,6 +1,3 @@
-
-from .gridappsd_integration import GridAPPSDSimIntegration
 from.energyplus_integration import EnergyPlusSimIntegration
 
-
-__all__ = ['GridAPPSDSimIntegration', 'EnergyPlusSimIntegration']
+__all__ = ['GridAPPSDSimIntegration']
